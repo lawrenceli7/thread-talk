08da3f595ecc1fe5929c7dcd2c140d0900c650ef
Lawrence Li <lawrenceli3202@gmail.com>
2024-07-31 02:46:40 -0400

    feat: finish signup and login modal components

--- src/components/modal/auth/index.tsx
+++ src/components/modal/auth/index.tsx
@@ -1,5 +1,6 @@
 import { authModalState } from "@/atoms/authModalAtom";
 import {
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,20 +10,44 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInputs";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+
   const handleClose = () => {
     setModalState((prev) => ({ ...prev, open: false }));
   };
+
   return (
     <>
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader textAlign="center">
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Modal Body</ModalBody>
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            pb={6}
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+            >
+              {/* <OAuthButtons /> */}
+              <AuthInputs />
+              {/* <ResetPassword /> */}
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
--- src/components/nav/content/AuthButtons.tsx
+++ src/components/nav/content/AuthButtons.tsx
@@ -1,10 +1,11 @@
+import { authModalState } from "@/atoms/authModalAtom";
 import { Button } from "@chakra-ui/react";
 import React from "react";
 import { useSetRecoilState } from "recoil";
-import { authModalState } from "@/atoms/authModalAtom";
 
 const AuthButtons: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <>
       <Button
