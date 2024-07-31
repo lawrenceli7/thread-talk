ac36428a2bf0921e87b1551e95f79a89c4ace49b
Lawrence Li <lawrenceli3202@gmail.com>
2024-07-30 22:32:40 -0400

    chore: create auth modal

--- src/components/nav/content/index.tsx
+++ src/components/nav/content/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
 import AuthButtons from "./AuthButtons";
+import AutoModal from "../../modal/auth";
 
 type RightContentProps = {
   user: any;
@@ -9,7 +10,7 @@ type RightContentProps = {
 const RightContent: React.FC<RightContentProps> = () => {
   return (
     <>
-      {/* <AutoModal /> */}
+      <AutoModal />
       <Flex justify="center" align="center">
         <AuthButtons />
       </Flex>
