34003f6f82f4ca33558db320a114571b8475d857
Lawrence Li <lawrenceli3202@gmail.com>
2024-07-31 23:48:42 -0400

    feat: add button to logout if user is already logged in

--- src/components/nav/content/index.tsx
+++ src/components/nav/content/index.tsx
@@ -1,18 +1,24 @@
+import { auth } from "@/firebase/clientApp";
+import { Button, Flex } from "@chakra-ui/react";
+import { signOut } from "firebase/auth";
 import React from "react";
-import { Flex } from "@chakra-ui/react";
-import AuthButtons from "./AuthButtons";
 import AutoModal from "../../modal/auth";
+import AuthButtons from "./AuthButtons";
 
 type RightContentProps = {
   user: any;
 };
 
-const RightContent: React.FC<RightContentProps> = () => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
       <AutoModal />
       <Flex justify="center" align="center">
-        <AuthButtons />
+        {user ? (
+          <Button onClick={() => signOut(auth)}>Logout</Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
