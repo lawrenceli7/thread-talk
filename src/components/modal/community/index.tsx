22e0fc0f5b2540e6418ba0ace12b875709ec2fc8
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-01 22:53:49 -0400

    feat: create basic modal for community

---
+++ src/components/modal/community/index.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+
+type CreateCommunityModalProps = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
+  open,
+  handleClose,
+}) => {
+  return (
+    <>
+      <Modal isOpen={open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>modal body</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Create Community</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default CreateCommunityModal;
