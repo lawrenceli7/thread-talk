d859dacc9513fcb71224cf84cdaa3abefff34b15
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-03 22:10:09 -0400

    feat: implement multiple forms for tab component

--- src/pages/r/[communityId]/Submit.tsx
+++ src/pages/r/[communityId]/Submit.tsx
@@ -1,6 +1,19 @@
+import PageContent from "@/components/layout/PageContent";
+import NewPostForm from "@/components/posts/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const SubmitPostPage: React.FC = () => {
-  return <div>submit page</div>;
+  return (
+    <PageContent>
+      <>
+        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text>Create a post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <></>
+    </PageContent>
+  );
 };
 export default SubmitPostPage;
