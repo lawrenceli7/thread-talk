9493e9c62b2cb56f321f7d02fa85e84740251ea5
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-07 14:10:04 -0400

    feat: implement home feed visibility for only log out users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import { communityState } from "@/atoms/communitiesAtom";
+import { Post } from "@/atoms/postsAtom";
+import CreatePostLink from "@/components/community/CreatePostLink";
 import PageContent from "@/components/layout/PageContent";
-import { auth } from "@/firebase/clientApp";
+import PostItem from "@/components/posts/PostItem";
+import PostLoader from "@/components/posts/PostLoader";
+import { auth, firestore } from "@/firebase/clientApp";
+import usePosts from "@/hooks/usePosts";
+import { Stack } from "@chakra-ui/react";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
@@ -9,13 +16,30 @@ export default function Home() {
   const [user, loadingUser] = useAuthState(auth);
   const communityStateValue = useRecoilState(communityState);
   const [loading, setLoading] = useState(false);
+  const {
+    setPostStateValue,
+    postStateValue,
+    onSelectPost,
+    onDeletePost,
+    onVote,
+  } = usePosts();
 
   const buildUserHomeFeed = () => {};
 
-  const buildNoUserHomeFeed = () => {
+  const buildNoUserHomeFeed = async () => {
     setLoading(true);
 
     try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+      setPostStateValue((prev) => ({ ...prev, posts: posts as Post[] }));
     } catch (error: any) {
       console.error("Error building no user home feed: ", error);
     }
@@ -32,7 +56,31 @@ export default function Home() {
 
   return (
     <PageContent>
-      <></>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                onSelectPost={onSelectPost}
+                onDelete={onDeletePost}
+                onVote={onVote}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue
+                }
+                userIsCreator={user?.uid === post.creatorId}
+                homePage
+              />
+            ))}
+          </Stack>
+        )}
+      </>
       <></>
     </PageContent>
   );
