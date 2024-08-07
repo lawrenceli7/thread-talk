88da56ba6ed978a6268a75a3b2a3cbac0cb4c538
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-07 17:00:33 -0400

    fix: fix children props for layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../nav";
 
-const Layout: React.FC = ({ children }) => {
+const Layout: React.FC = ({ children }: React.PropsWithChildren<{}>) => {
   return (
     <>
       <Navbar />
